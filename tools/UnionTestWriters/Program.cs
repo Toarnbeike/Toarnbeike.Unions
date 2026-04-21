e0780b08f43ba2a3de7d16b9012b377d136a1e0d
Toarnbeike <toarnbeike@outlook.com>
2026-04-21 12:03:22 +0200

    Renamed tools to Writers, and used string.Join fluent extensions. Also added a WriterLibrary for the shared logic between UnionFileWriters en UnionTestFileWriters

---
+++ tools/UnionTestWriters/Program.cs
@@ -0,0 +1,62 @@
+﻿using UnionTestWriters.Writers;
+
+const int minArity = 2;
+const int maxArity = 15; // values above 15 are untested, increase at own risk.
+
+Console.WriteLine("Welcome to the Generic Union Test Writer");
+Console.WriteLine($"Please indicate the maximum arity to write test for (between {minArity} and {maxArity}):");
+
+while (true)
+{
+    if (int.TryParse(Console.ReadLine(), out var parsedArity) && parsedArity is >= minArity and <= maxArity)
+    {
+        return GenerateTests(parsedArity);
+    }
+    Console.WriteLine($"Invalid input. Please enter a number between {minArity} and {maxArity}:");
+}
+
+static int GenerateTests(int maxArity)
+{
+    var range = Enumerable.Range(2, maxArity - 1).ToArray();  // arities from 2 to upperBound inclusive
+    var outputRoot = "../../../../../tests/Unions.Tests"; // location to write generated test files
+
+    Console.WriteLine("\nGenerating union test types");
+    var unionTestTypes = UnionTestTypeGenerator.Generate(maxArity);
+    File.WriteAllText(Path.Join(outputRoot, "TestTypes/UnionTestTypes.cs"), unionTestTypes);
+
+
+    foreach (var arity in range)
+    {
+        Console.WriteLine($"Write tests for arity {arity}");
+
+        var unionTests = UnionTestWriter.WriteFile(arity);
+        File.WriteAllText(Path.Join(outputRoot, $"Union{arity:D2}Tests.cs"), unionTests);
+
+        var matchTests = MatchExtensionsTestsWriter.WriteFile(arity);
+        File.WriteAllText(Path.Join(outputRoot, $"Extensions/Match/Match{arity:D2}Tests.cs"), matchTests);
+
+        var switchTests = SwitchExtensionsTestsGenerator.WriteFile(arity);
+        File.WriteAllText(Path.Join(outputRoot, $"Extensions/Switch/Switch{arity:D2}Tests.cs"), switchTests);
+
+        var mapFullTests = MapFullExtensionsTestsWriter.WriteFile(arity);
+        var mapPartialTests = MapPartialExtensionsTestsWriter.WriteFile(arity);
+        File.WriteAllText(Path.Join(outputRoot, $"Extensions/Map/Map{arity:D2}FullTests.cs"), mapFullTests);
+        File.WriteAllText(Path.Join(outputRoot, $"Extensions/Map/Map{arity:D2}PartialTests.cs"), mapPartialTests);
+
+        var bindPartialTests = BindExtensionsTestsWriter.WriteFile(arity);
+        File.WriteAllText(Path.Join(outputRoot, $"Extensions/Bind/Bind{arity:D2}Tests.cs"), bindPartialTests);
+
+        var tapTests = TapExtensionsTestsWriter.WriteFile(arity);
+        File.WriteAllText(Path.Join(outputRoot, $"Extensions/Tap/Tap{arity:D2}Tests.cs"), tapTests);
+
+        var collectionExtensionsTests = CollectionExtensionsTestsWriter.WriteFile(arity);
+        File.WriteAllText(Path.Join(outputRoot, $"Collections/CollectionExtensions{arity:D2}Tests.cs"), collectionExtensionsTests);
+
+        var testExtensionsTests = TestExtensionsTestsWriter.WriteFile(arity);
+        File.WriteAllText(Path.Join(outputRoot, $"TestExtensions/TestExtensions{arity:D2}Tests.cs"), testExtensionsTests);
+    }
+
+    Console.WriteLine("\nTest generation completed successfully.");
+
+    return 0;
+}
---
+++ tools/UnionWriters/Program.cs
@@ -0,0 +1,60 @@
+﻿using UnionWriters.Writers;
+
+const int minArity = 2;
+const int maxArity = 15; // values above 15 are untested, increase at own risk.
+
+Console.WriteLine("Welcome to the Generic Union Writer");
+Console.WriteLine($"Please indicate the maximum arity to write (between {minArity} and {maxArity}):");
+
+while (true)
+{
+    if (int.TryParse(Console.ReadLine(), out var parsedArity) && parsedArity is >= minArity and <= maxArity)
+    {
+        return WriteFiles(parsedArity);
+    }
+    Console.WriteLine($"Invalid input. Please enter a number between {minArity} and {maxArity}:");
+}
+
+static int WriteFiles(int upperBound)
+{
+    var range = Enumerable.Range(2, upperBound - 1).ToArray(); // arities from 2 to upperBound inclusive
+    var outputRoot = "../../../../../src/Unions"; // location to write generated files
+
+    Console.WriteLine("Writing unions");
+    var unions = UnionWriter.WriteFile(range);
+    File.WriteAllText(Path.Join(outputRoot, "Union.cs"), unions);
+
+    Console.WriteLine("Writing match extensions");
+    var matches = MatchExtensionsWriter.WriteFile(range);
+    File.WriteAllText(Path.Join(outputRoot, "Extensions/MatchExtensions.cs"), matches);
+
+    Console.WriteLine("Writing switch extensions");
+    var switches = SwitchExtensionsWriter.WriteFile(range);
+    File.WriteAllText(Path.Join(outputRoot, "Extensions/SwitchExtensions.cs"), switches);
+
+    Console.WriteLine("Writing map extensions");
+    var mapFull = MapFullExtensionsWriter.WriteFile(range);
+    var mapPartial = MapPartialExtensionsWriter.WriteFile(range);
+    File.WriteAllText(Path.Join(outputRoot, "Extensions/MapExtensions.Full.cs"), mapFull);
+    File.WriteAllText(Path.Join(outputRoot, "Extensions/MapExtensions.Partial.cs"), mapPartial);
+
+    Console.WriteLine("Writing bind extensions");
+    var binds = BindExtensionsWriter.WriteFile(range);
+    File.WriteAllText(Path.Join(outputRoot, "Extensions/BindExtensions.cs"), binds);
+
+    Console.WriteLine("Writing tap extensions");
+    var taps = TapExtensionsWriter.WriteFile(range);
+    File.WriteAllText(Path.Join(outputRoot, "Extensions/TapExtensions.cs"), taps);
+
+    Console.WriteLine("Writing collection extensions");
+    var collectionExtensions = CollectionExtensionsWriter.WriteFile(range);
+    File.WriteAllText(Path.Join(outputRoot, "Collections/CollectionExtensions.cs"), collectionExtensions);
+
+    Console.WriteLine("Writing test extensions");
+    var testExtensions = TestExtensionsWriter.WriteFile(range);
+    File.WriteAllText(Path.Join(outputRoot, "TestExtensions/TestExtensions.cs"), testExtensions);
+
+    Console.WriteLine("Writing files succeeded.");
+
+    return 0;
+}
